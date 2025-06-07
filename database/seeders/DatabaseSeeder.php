2d781c6c1463a47bd1ab7188f2ee428a16003fc5
Massika-Ke <wamwoka99@gmail.com>
2025-06-07 18:29:39 +0300

    Faker - seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(PostsTableSeeder::class);
-        
+        // $this->call(PostsTableSeeder::class);
+        Post::factory(100)->create();
     }
 }
