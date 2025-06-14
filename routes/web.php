758a571bc5938286350a609465b9604fc6585720
Massika-Ke <wamwoka99@gmail.com>
2025-06-14 22:35:58 +0300

    Breeze-auth

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,20 @@
 <?php
 
-use App\Http\Controllers\FallbackController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::prefix("/blog")->group(function () {
-    // GET
-    Route::get('/create', [PostsController::class,'create'])->name('blog.create');
-    Route::get('/', [PostsController::class,'index'])->name('blog.index');
-    Route::get('/{id}', [PostsController::class,'show'])->name('blog.show');
-
-    // POST
-    Route::post('/', [PostsController::class,'store'])->name('blog.store');
-
-    // PUT / PATCH
-    Route::get('/edit/{id}', [PostsController::class,'edit'])->name('blog.edit');
-    Route::patch('/{id}', [PostsController::class,'update'])->name('blog.update');
-
-    // DELETE
-    Route::delete('/{id}', [PostsController::class,'destroy'])->name('blog.destroy');
-
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Multiple HTTP verbs
-// Route::match(['GET', 'POST'], 'blog', [PostsController::class,'index']);
-// Route for invoke method
-Route::get('/', HomeController::class); 
-
-// Route::resource('blog', PostsController::class);
-
-// fallback route
-Route::fallback(FallbackController::class);
+require __DIR__.'/auth.php';
